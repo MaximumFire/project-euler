b93adc6e7e5f2b10865b821408e930fa38a46f3d
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-07-17 22:07:30 +0100

    compressed this one for lols

--- 1-50/31/main.py
+++ 1-50/31/main.py
@@ -1,6 +1,4 @@
-ways = [0 for i in range(201)]
-ways[0] = 1
-
+ways = [(1 if i == 0 else 0) for i in range(201)]
 for x in [1, 2, 5, 10, 20, 50, 100, 200]:
     for i in range(x, 201):
         ways[i] += ways[i-x]
