d9f74b8c23c596a7ec2891c803d1a23d5c21e16d
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-07-28 13:43:35 +0100

    Update main.py

--- 51-100/61/main.py
+++ 51-100/61/main.py
@@ -21,10 +21,10 @@ def semi_check(x):
         return False
     return True
 
-triangles = [(n*(n+1))//2 for n in range(1, 142) if 10000 > (n*(n+1))//2 >= 1000]
-squares = [(n**2) for n in range(1, 102) if 10000 > (n**2) > 1000]
+triangles = [(n*(n+1))//2 for n in range(1, 142) if 10000 > (n*(n+1))//2 >= 1000][::-1]
+squares = [(n**2) for n in range(1, 102) if 10000 > (n**2) > 1000][::-1]
 pentagons = [(n*(3*n-1))//2 for n in range(1, 83) if 10000 > (n*(3*n-1))//2 >= 1000]
-hexagons = [(n*(2*n-1)) for n in range(1, 72) if 10000 > (n*(2*n-1)) >= 1000]
+hexagons = [(n*(2*n-1)) for n in range(1, 72) if 10000 > (n*(2*n-1)) >= 1000][::-1]
 heptagons = [(n*(5*n-3))//2 for n in range(1, 65) if 10000 > (n*(5*n-3))//2 >= 1000]
 octagons = [(n*(3*n-2)) for n in range(1, 60) if 10000 > (n*(3*n-2)) >= 1000]
 
