d40ce566c460fd66a8c5eb788f62d80aad830ed5
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-10-25 18:16:50 +0100

    Update prime_gen.py

--- prime_gen.py
+++ prime_gen.py
@@ -2,6 +2,7 @@ import json
 
 def primes(n):
     prime = [1 for i in range(n+1)]
+    prime[0], prime[1] = 0, 0
     p = 2
     while(p * p <= n):
         if (prime[p] == 1):
@@ -10,6 +11,6 @@ def primes(n):
         p += 1
     return prime
 
-with open("c:/Users/conno/Onedrive/Documents/Github/project-euler/primes_billion.json", "w+") as f:
-    f.write(json.dumps({"primes": primes(1000_000_000)}))
+with open("c:/Users/conno/Onedrive/Documents/Github/project-euler/primes_million.json", "w+") as f:
+    f.write(json.dumps({"primes": primes(1_000_000)}))
 
